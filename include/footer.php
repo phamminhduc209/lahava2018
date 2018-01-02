c1ae69d39b22cff5a634603bde829898fde9c680
Ducpm <minhducpham.it@gmail.com>
2018-01-02 16:58:24 +0700

    Duc

---
+++ include/footer.php
@@ -0,0 +1,178 @@
+<footer class="footer-container">
+        <div class="footer inner">
+            <div class="clearfix footer-info">
+                <div class="footer-col">
+                    <h2 class="footer-title">LAHAVA</h2>
+                    <div class="footer-links">
+                        <ul>
+                            <li><a href="about.html">Giới thiệu</a></li>
+                            <li><a href="customer-service.html">Hướng dẫn mua hàng</a></li>
+                            <li class="last privacy"><a href="privacy-policy.html">Chính sách bảo mật</a></li>
+                        </ul>
+                        <div class="footer-links">
+                            <ul class="links">
+                                <li class="first"><a href="blog.html" title="Blog" class="top-link-blog">Blog</a></li>
+                                <li><a href="xu-huong.html" title="Nhu cầu tìm kiếm">Nhu cầu tìm kiếm</a></li>
+                                <li><a href="contact.html" title="Liên hệ với chúng tôi">Liên hệ với chúng tôi</a></li>
+                                <li class="last"><a href="huong-dan-lay-so-do.html" target="_blank" title="Hướng dẫn lấy số đo">Hướng dẫn lấy số đo</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer-col">
+                    <h2 class="footer-title">Hệ thống Website</h2>
+                    <div class="footer-links">
+                        <ul>
+                            <li><a href="bo-suu-tap.html" target="_blank">Bộ sưu tập</a></li>
+                            <li><a title="Đầm Dạ Hội" href="http://damdahoi.com.vn" target="_blank">Đầm dạ hội</a></li>
+                            <li class="last privacy"><a title="Áo Dài Cưới" href="http://mayaodaidep.com/ao-dai-cuoi.htm" target="_blank">Áo dài cưới</a></li>
+                            <li><a title="Áo dài nam" href="http://www.aodainam.vn" target="_blank">Áo dài nam</a></li>
+                            <li class="last privacy"><a title="May ao cuoi" href="http://mayaocuoidep.vn" target="_blank">May áo cưới</a></li>
+                        </ul>
+                        <ul class="links">
+                            <li class="first"><a title="Đầm ren đẹp" href="http://damrendep.com" target="_blank">Đầm váy ren</a></li>
+                            <li><a title="Tổ chức từ thiện LAHAVA" href="http://charity-lahava.com/" target="_blank">Charity LAHAVA</a></li>
+                            <li><a title="Việc làm thời trang" href="tuyen-dung.html" target="_blank"><span class="_red">Tuyển dụng</span></a></li>
+                        </ul>
+                    </div>                            
+                </div>
+                <div class="footer-col">
+                    <h2 class="footer-title">Liên hệ</h2>
+                    <div class="footer-contacts">
+                        <div class="block block-subscribe clearfix mb20">
+                            <div class="block-title"><strong><span>Bản tin điện tử</span></strong></div>
+                            <form action="https://www.lahava.vn/newsletter/subscriber/new/" method="post" id="newsletter-validate-detail">
+                                <div class="block-content">
+                                    <div class="form-subscribe-header">
+                                        <label for="newsletter">Đăng ký nhận tin khuyến mãi:</label>
+                                    </div>
+                                    <div class="input-box">
+                                       <input type="text" name="email" id="newsletter" title="Đăng ký nhận tin khuyến mãi" class="input-text required-entry validate-email" placeholder="Nhập email của bạn">
+                                    </div>
+                                    <div class="actions clearfix">
+                                        <button type="submit" title="Subscribe" class="button"><span><span>Gửi đi</span></span></button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="contact-info">
+                            <p class="contact-info-title">Cty Cổ phần Thời trang LAHAVA</p>
+                            <p class="mb5">84 Nguyễn Văn Trỗi, Phường 8<br>Quận Phú nhuận, TPHCM</p>
+                            <div class="mb5">
+                                <p><span class="txt-underline">Phone:</span> 0286.2772777</p>
+                                <p><span class="txt-underline">Hotline:</span> 0975.997.567 - 0942.926.456</p>
+                                <p><span class="txt-underline">E-mail:</span> lananhthoitrang@gmail.com</p>
+                            </div>
+                            <div class="mb5">
+                                <p><span class="txt-underline">Than phiền dịch vụ:</span> 0939.333.046</p>
+                                <p><span class="txt-underline">Email:</span> litz.truong@gmail.com</p>
+                            </div>
+                            <div>
+                                <p><span class="txt-underline">Thời gian làm việc:</span></p>
+                                <p style="font-size: x-small;">
+                                    <strong>T2 - T7: &nbsp; &nbsp; &nbsp;9:00 - 21:00<br>
+                                        Chủ nhật: &nbsp;9:00 - 19:00&nbsp;<br>
+                                    </strong>
+                                </p>
+                                <p style="font-size: x-small; color: #800000;">Nghỉ trưa: 12:00 - 13:00</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a href="#" id="scroll-to-top">&nbsp;</a>
+            <div class="footer-menu">
+                <ul class="links">
+                    <li><a href="cart.html" title="Giỏ hàng" class="top-link-cart">Giỏ hàng</a></li>
+                    <li><a href="#" title="Đặt hàng" class="top-link-checkout">Đặt hàng</a></li>
+                </ul>
+                <div class="copyrights">LAHAVA © 2017  -  SINCE 2007</div>        
+            </div>
+        </div>
+        <div class="quick_order"><a><img onclick="showMessageBox()" src="images/common/message-icon.png" alt="lien-he"></a></div>
+        <div class="alert_helper" style="display: none;">
+            <div>Chúng tôi có thể giúp gì cho bạn?</div>
+            <div class="arrow-right"></div>
+        </div>
+        <div class="wrap_hidden_message_content">
+            <div class="hidden_message_header">Thông tin<div class="hidden_close_icon"><img src="images/common/icon_close.png" alt="close_icon" width="15px"></div></div>
+            <div class="hidden_message_content">
+                <p>
+                    -:- Sản phẩm không cho thuê.<br>
+                    -:- Thời gian may từ 2 đến 3 tuần.<br>
+                    -:- Không bán vải, không nhận gia công.<br>
+                    -:- Gửi hàng trong nước và nước ngoài.<br>
+                    -:- Gửi mẫu tư vấn, báo giá <span style="color: #800000;">
+                    <a href="order.html" target="_blank"><span style="color: #800000;"><strong>tại đây »</strong></span></a></span><br>-:- Hướng dẫn lấy số đo <span style="color: #800000;">
+                    <a href="huong-dan-lay-so-do.html" target="_blank"><span style="color: #800000;"><strong>tại đây »</strong></span></a></span><br>-:- Hướng dẫn thanh toán <span style="color: #800000;"><strong>
+                    <a href="customer-service.html" target="_blank"><span style="color: #800000;">tại dây »</span></a></strong></span>
+                </p>
+                <h2><a id="call_popup2" href="#lahava_feedback"><img class="fancybox_click" style="display: block; margin-left: auto; margin-right: auto;" src="images/common/gopy.jpg" alt=""></a></h2>
+                <h2>LAHAVA</h2>
+                <p>84 Nguyễn Văn Trỗi, P.8, Phú nhuận, HCM<br>Email: lananhthoitrang@gmail.com<br>Hotline: 0286.2772777 - 0942.926.456</p>
+                <p><span style="font-size: x-small;">T2 - T7: &nbsp; &nbsp;9:00 - 21:00</span><br><span style="font-size: x-small;">Chủ nhật: 9:00 - 19:00<br><span style="color: #800000;">Nghỉ trưa: 12:00 - 13:00</span></span></p>
+                <p class="center"><span style="color: #808080;"><em><span style="font-size: x-small;">Bạn đang tìm hiểu tại một dịch vụ tốt nhất!</span></em></span></p>
+                <p class="center"><img src="images/common/Border_line.png" alt=""><span style="font-size: x-small;"><br></span></p>  
+            </div>
+        </div>
+        
+        <div id="lahava_feedback" class="lahava_feedback">
+            <div class="feedback_title"><p>Góp ý - Than phiền</p></div>
+            <div class="feedback_content">
+                <p><strong>Theo bạn, Chúng tôi nên cải thiện điều gì về Sản phẩm, Dịch vụ và Website?</strong><br><strong>Hãy cho chúng tôi biết những điều khiến bạn chưa hài lòng ở LAHAVA!</strong></p>
+                <p><em><strong><span style="font-size: 11px; font-family: arial, helvetica, sans-serif;">Rất hân hạnh được lắng nghe ý kiến của bạn!</span></strong></em></p>
+            </div>
+            <div class="feedback_questions">
+                <div id="webform_3_form">
+                    <form action="#" class="webforms-feedback" name="webform_3" method="post" enctype="application/x-www-form-urlencoded">
+                        <input type="hidden" name="submitWebform_3" value="1">
+                        <input type="hidden" name="webform_id" value="3">
+                        <div id="fieldset_0" class="fieldset fieldset-0">
+                            <ul class="form-list">
+                                <li class="fields ">
+                                    <div id="field_68" class="field webforms-fields-68">
+                                        <label for="field_68" class="required"><em>*</em>feebackform</label>
+                                        <div class="input-box">
+                                            <textarea name="field[68]" id="field[68]" class="input-text required-entry"></textarea>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="fieldset">
+                            <h2 class="legend">Captcha</h2>
+                            <ul class="form-list">
+                                <li class="fields">
+                                    <div class="field">
+                                        <script type="text/javascript">
+                                            var RecaptchaOptions = {"theme":"white","lang":"en"};
+                                        </script>
+                                        <script type="text/javascript" src="https://api.recaptcha.net/challenge?k=6Le8AzkUAAAAAI6a0wXXYkUjmTo5pwz-6369DutK"></script>
+                                        <noscript>
+                                            <iframe src="https://api.recaptcha.net/noscript?k=6Le8AzkUAAAAAI6a0wXXYkUjmTo5pwz-6369DutK"
+                                            height="300" width="500" frameborder="0"></iframe><br>
+                                            <textarea name="recaptcha_challenge_field" rows="3" cols="40">
+                                            </textarea>
+                                            <input type="hidden" name="recaptcha_response_field"
+                                            value="manual_challenge">
+                                        </noscript>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                        <style>.honeypot{display:none}</style>
+                        <div class="honeypot">
+                            <input type="text" name="message" value=""/>
+                        </div>
+                        <div class="buttons-set clearfix">
+                            <p class="required">* Phải nhập thông tin</p>
+                            <button type="button" class="button validation-passed" id="webform_3_submit_button" onclick="webform_3_submit()" title="submit"><span><span>Gửi đi</span></span></button>
+                            <span class="please-wait" id="webform_3_sending_data" style="display:none;">
+                                <img src="images/common/opc-ajax-loader.gif" alt="Sending..." title="Sending..." class="v-middle"><span id="webform_3_progress_text">Sending...</span>
+                            </span>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </footer>
