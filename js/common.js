27320f36680eae4c297d85ca5e1af597e9acea8e
Duc Pham <minhducpham.it@gmail.com>
2018-01-29 06:32:42 +0700

    Duc

--- js/common.js
+++ js/common.js
@@ -76,24 +76,17 @@
     });
   });
 
-  function close_accordion_section() {
-    $('.accordion .accordion-section-title').removeClass('active');
-    $('.accordion .accordion-section-content').slideUp(300).removeClass('open');
-  }
-  $('.accordion-section-title').click(function(e) {
-      // Grab current anchor value
-    var currentAttrValue = $(this).attr('href');
-    if($(e.target).is('.active')) {
-      close_accordion_section();
-    } else {
-      close_accordion_section();
-      // Add active class to section title
-      $(this).addClass('active');
-      // Open up the hidden content panel
-      $('.accordion ' + currentAttrValue).slideDown(300).addClass('open'); 
-    }
-    e.preventDefault();
-  });
+  $('.show-more').on('click', function() {
+    $(this).parent().addClass('all');
+  })
 
+  function toggleIcon(e) {
+    $(e.target)
+      .prev('.panel-heading')
+      .find(".more-less")
+      .toggleClass('fa-caret-right fa-caret-down');
+  }
+  $('.panel-group').on('hidden.bs.collapse', toggleIcon);
+  $('.panel-group').on('shown.bs.collapse', toggleIcon);
 
 })(jQuery); // End of use strict
